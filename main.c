8d50c5b1ea97b93d5bd05f11803f93e5197c3e97
grumpyCello <kfoniciello@gmail.com>
2020-03-19 15:12:03 -0600

    working on stuff

--- main.c
+++ main.c
@@ -1,40 +1,66 @@
 #include "msp.h"
 #include "stdbool.h"
+//#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+#include <stdint.h>
+#include <string.h>
+
+//#define CAR
+#define REMOTE
 
 #define CLOCK_SPEED 3000000
 #define BAUD_RATE 9600
 
-// Signals to control car
-#define SIGNAL_MOTOR_1_STOP     0x10
-#define SIGNAL_MOTOR_1_FORWARD  0x11
-#define SIGNAL_MOTOR_1_BACKWARD 0x12
-#define SIGNAL_MOTOR_2_STOP     0x20
-#define SIGNAL_MOTOR_2_FORWARD  0x21
-#define SIGNAL_MOTOR_2_BACKWARD 0x22
+#define DELAY 300
 
-// macro defines for the Car
-    // on port 3
-#define RECEIVER BIT3
-    // on port 4
-#define MOTOR_1_FORWARD  BIT2
-#define MOTOR_1_BACKWARD BIT3
-#define MOTOR_1_STOP     ~(BIT2 | BIT3)
-#define MOTOR_2_FORWARD  BIT4
-#define MOTOR_2_BACKWARD BIT5
-#define MOTOR_2_STOP     ~(BIT4 | BIT5)
+
+#define RT_PORT     P3
+#define TRANSMITTER BIT2
+#define RECEIVER    BIT3
+// Signals to control car
+#define SIGNAL_STOP     0x10
+#define SIGNAL_FORWARD  0x11
+#define SIGNAL_BACKWARD 0x12
+#define SIGNAL_LEFT     0x13
+#define SIGNAL_RIGHT    0x14
+
 
 // macro defines for the remote
-    // on port 3
-#define TRANSMITTER BIT2
+#ifdef REMOTE
+    #define BTN_PORT P4
+    #define FORWARD  BIT1
+    #define BACKWARD BIT2
+    #define LEFT     BIT3
+    #define RIGHT    BIT4
+
+    void setup(DIO_PORT_Even_Interruptable_Type*, uint16_t, uint16_t, DIO_PORT_Odd_Interruptable_Type*, uint16_t, uint16_t);
+    void send(char);
+    char dataReceived(void);
+#endif // REMOTE
 
 
-void setup(DIO_PORT_Even_Interruptable_Type*, uint16_t, uint16_t, DIO_PORT_Odd_Interruptable_Type*, uint16_t, uint16_t);
-void initHMslave(void);
-void handleClientBLE(void);
-void sendData(char *data, char *ack, char *err, const int timeout);
-void delay_ms(uint16_t ms);
-void send(char);
-char dataReceived(void);
+// macro defines for the Car
+#ifdef CAR
+        // on port 3
+#define MOTOR_1 P3
+        // on port 4
+    #define MOTOR_1_FORWARD  BIT2
+    #define MOTOR_1_BACKWARD BIT3
+    #define MOTOR_1_STOP     ~(BIT2 | BIT3)
+    #define MOTOR_2_FORWARD  BIT4
+    #define MOTOR_2_BACKWARD BIT5
+    #define MOTOR_2_STOP     ~(BIT4 | BIT5)
+
+
+    char RXData[1000] = "";
+    int buf_ind = 0, i = 0;
+
+    void setup(DIO_PORT_Even_Interruptable_Type*, uint16_t, uint16_t, DIO_PORT_Odd_Interruptable_Type*, uint16_t, uint16_t);
+    void initHMslave(void);
+    void handleClientBLE(void);
+    void sendData(char *data, char *ack, char *err, const int timeout);
+    void delay_ms(uint16_t ms);
+    void send(char);
+    char dataReceived(void);
 
 /********************
  * Car main.c code
@@ -122,21 +148,21 @@ void initHMslave(void) {
 }
 
 void handleClientBLE(void) {
-//    char receivedData[1000] = "";
-//
-//    if(buf_ind>0) {
-//        delay_ms(1000);
-//        strncpy(receivedData, RXData, buf_ind);
-//        memset(RXData, 0, sizeof(RXData));
-//        buf_ind = 0;
-//    }
-//
-//    if(strstr(receivedData, "D") != NULL || strstr(receivedData, "d") != NULL) {
-//        // do something
-//    }
-//    else if(strstr(receivedData, "S") != NULL || strstr(receivedData, "s") != NULL) {
-//        // do something else
-//    }
+    char receivedData[1000] = "";
+
+    if(buf_ind>0) {
+        delay_ms(1000);
+        strncpy(receivedData, RXData, buf_ind);
+        memset(RXData, 0, sizeof(RXData));
+        buf_ind = 0;
+    }
+
+    if(strstr(receivedData, "D") != NULL || strstr(receivedData, "d") != NULL) {
+        // do something
+    }
+    else if(strstr(receivedData, "S") != NULL || strstr(receivedData, "s") != NULL) {
+        // do something else
+    }
 }
 
 /**
@@ -167,36 +193,131 @@ char dataReceived(void) {
  * oh, it sets up the HM10 for use
  * like command line for the HM10 :)
  ***************************/
-void sendData(char *data, char * ack, chat *err, const int timeout){
-    long int time = 0;
-    memset(RXData, 0 sizeof(RXData));
-    buf_ind = 0;
-
-    while (*data != 0x00)
-        UART_transmitData(EUSCI_A2_BASE, *data++);
-
-    while(timeout > time){
-        time++;
-        delay_ms(1);
-        if(strstr(RXData, ack) != NULL) break;
-        elseif(strstr(RXData, err) != NULL) while(1);
-    }
-    delay_ms(100);
+void sendData(char *data, char *ack, char *err, const int timeout){
+//    long int time = 0;
+//    memset(RXData, 0, sizeof(RXData));
+//    buf_ind = 0;
+//
+//    while (*data != 0x00)
+//        UART_transmitData(EUSCI_A2_BASE, *data++);
+//
+//    while(timeout > time){
+//        time++;
+//        delay_ms(1);
+//        if(strstr(RXData, ack) != NULL) break;
+//        else if(strstr(RXData, err) != NULL) while(1);
+//    }
+//    delay_ms(100);
 }
 
+/**
+ * DELAY_MS
+ */
+void delay_ms(uint16_t ms){
+  uint16_t delay;
+  volatile uint32_t i;
+
+  for (delay = ms; delay >0 ; delay--)
+    for (i=1200; i >0;i--);
+}
+#endif // CAR
+
+#ifdef REMOTE
 /********************
  * Remote main.c code
  ********************/
-/*#define FORWARD BIT7
-#define BACKWARD BIT6
-#define LEFT BIT4
-#define Right BIT5
 void main(void)
 {
-    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
+    setup(BTN_PORT, FORWARD | BACKWARD, LEFT | RIGHT,
+          RT_PORT, RECEIVER, TRANSMITTER);
 
+    while(true) {
+//      Delay for switch debouncing
+        for (i = 0; i < DELAY; i++) {}
+
+
+//  1)  Read BTNs
+        bool f_Pressed = (BTN_PORT->IN & FORWARD) == FORWARD;
+        bool b_Pressed = (BTN_PORT->IN & BACKWARD) == BACKWARD;
+        bool l_Pressed = (BTN_PORT->IN & LEFT) == LEFT;
+        bool r_Pressed = (BTN_PORT->IN & RIGHT) == RIGHT;
+
+        if (f_Pressed || b_Pressed || l_Pressed || r_Pressed) {
+            if (f_Pressed) {
+                send(SIGNAL_FORWARD);
+            } else if (b_Pressed) {
+                send(SIGNAL_BACKWARD);
+            } else if (l_Pressed) {
+                send(SIGNAL_LEFT);
+            } else if (r_Pressed) {
+                send(SIGNAL_RIGHT);
+            }
+        } else {
+            send(SIGNAL_STOP);
+        }
+
+    }
 }
 
-*/
+/********************
+ * SETUP
+ ********************/
+void setup(
+    DIO_PORT_Even_Interruptable_Type* btnsPort, // I/O Port
+    uint16_t btnPin_1,
+    uint16_t btnPin_2,
+    DIO_PORT_Odd_Interruptable_Type* rtPort, // Receiver/Transmitter Port
+    uint16_t receiverPin,
+    uint16_t transmitterPin) {
+
+//    stop watchdog timer
+    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;
+
+//    Setup inputs
+    btnsPort->DIR &= ~(btnPin_1 | btnPin_2);
+
+
+//    Setup UART
+    EUSCI_A2->CTLW0  = 0x00c1; // = 0000 0000 1100 0001 bit settings
+    EUSCI_A2->BRW    = CLOCK_SPEED / BAUD_RATE;
+    EUSCI_A2->MCTLW &= ~BIT0;
+
+//    Setup P3.2 (receiver) and P3.3 (Transmitter)
+    rtPort->DIR  &= ~(receiverPin); // set receiver on P3 as input
+    rtPort->DIR  |= transmitterPin; // set transmitter on P3 as output
+    rtPort->REN  |= receiverPin;    // turn on receiver pull resister
+    rtPort->OUT  |= receiverPin;    // configure resistors as pull-up
+    rtPort->SEL0 |= transmitterPin|receiverPin;    // set for GPIO
+    rtPort->SEL1 &= ~(transmitterPin|receiverPin); // set for GPIO
+
+    EUSCI_A2->CTLW0 &= ~(BIT0);
+
+//    initHMslave();
+}
+
+/**************************
+ * SEND
+ * Transmits data to board2
+ **************************/
+void send(char data) {
+    while ((EUSCI_A2->IFG & BIT1) == 0);  // Busy.  Wait for previous output.
+    EUSCI_A2->TXBUF = data;                // Start transmission when IFG = 1.
+}
+
+/*****************************************************
+ * DATA_RECEIVED
+ * Reads the data received from board2 if there is any
+ * Return data read
+ *****************************************************/
+char dataReceived(void) {
+    if ((EUSCI_A2->IFG & BIT0) == 1) {
+       return ( (char) (EUSCI_A2->RXBUF) );
+    } else {
+        return 0xEE;
+    }
+}
+#endif // REMOTE
+
+
 
 
